c24739b2e30931efc8f3c497e17854aa090712af
AndersenCastanedaUniAndes <ad.castaneda2@uniandes.edu.co>
2024-02-24 21:24:18 -0500

    Se agrega base de monitoreo del servicio de registro

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -1,10 +1,29 @@
+import requests
 import redis
 import time
+
+def check_health():
+    try:
+        response = requests.get('http://127.0.0.1:5002/health')
+        if response.status_code == 200:
+            print("El servicio de registro está UP")
+            return True
+        else:
+            print("El servicio de registro está DOWN")
+            return False
+    except requests.exceptions.RequestException as e:
+        print("No se pudo conectar al servicio de registro")
+        return False
 
 def main():
     r = redis.Redis(host='redis', port=6379, db=0)
     while True:
-        r.publish('events', 'Ping')
+        if check_health():
+            r.publish('events', 'El servicio de registro está funcionando correctamente')
+        else:
+            # TODO: Implementar el reinicio del servicio
+            r.publish('events', 'El servicio de registro está caído, intentando reiniciar')
+
         time.sleep(1)
 
 if __name__ == '__main__':
--- registro/registro.py
+++ registro/registro.py
@@ -38,4 +38,4 @@ def desactivar_registro():
     })
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5002)
+    app.run(debug=True, host='127.0.0.1', port=5002)
